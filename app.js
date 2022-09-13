e7e7936c88768e5120ddf57d5334eca6f652730a
ivan2798 <inge1998@hotmail.com>
2022-09-13 15:15:18 -0500

    keywords

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-console.log("Hola mundo");
-alert("Hey, como estas");
+//console.log("Hola mundo");
+//alert("Hey, como estas");
+
+//palabras reservadas
+
+//keywords: var, let, const
+
+/*const nombre = "Ivan";
+let apellido = "Herrera";
+apellido = "Barrios";*/
+
+console.log();
