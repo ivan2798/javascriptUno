488564491c7fc65594561a239e1f6dd059b68cd2
ivan2798 <inge1998@hotmail.com>
2022-09-13 15:21:27 -0500

    == y ===

--- app.js
+++ app.js
@@ -3,10 +3,28 @@
 
 //palabras reservadas
 
-//keywords: var, let, const
+//keywords: var, let, const.
+//var es antiguo, mejor let
 
 /*const nombre = "Ivan";
 let apellido = "Herrera";
 apellido = "Barrios";*/
+/*
+let numero = 3;
+numero = 4;
+*/
 
-console.log();
+let completo = true;
+completo=false;
+
+let word = "alphabet";
+let alphabet="word";
+
+let y = 5;
+let x = y;
+
+let num1 = 2;
+let num2 = "2";
+
+console.log(num1==num2); //compara el valor de algo
+console.log(num1===num2); //compara el tipo tambien
